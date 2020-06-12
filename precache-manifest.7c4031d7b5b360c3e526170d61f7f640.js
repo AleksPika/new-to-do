d986a116bd6b5b15f5afaa20fb5255280ec715de
payoneercashout <56314279+payoneercashout@users.noreply.github.com>
2020-06-12 19:33:17 +0300

    add

---
+++ precache-manifest.7c4031d7b5b360c3e526170d61f7f640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adef9726fc6a1ff892c5cb876f39614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2aa815e01e452bb5cc9",
+    "url": "/static/css/main.f49f0cec.chunk.css"
+  },
+  {
+    "revision": "5ee12485f05289775cf9",
+    "url": "/static/js/2.b9569040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b9569040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2aa815e01e452bb5cc9",
+    "url": "/static/js/main.99b58892.chunk.js"
+  },
+  {
+    "revision": "37a0d35451170c581fd7",
+    "url": "/static/js/runtime-main.c1487c63.js"
+  }
+]);
